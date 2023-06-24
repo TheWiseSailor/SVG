beb7434434f631c92c476c8d949ff33b38c20114
TheWiseSailor <rudasilljay@gmail.com>
2023-06-24 17:24:31 -0400

    linking files/notes

---
+++ shapes.js
@@ -0,0 +1,7 @@
+const generateLogoSvg = (text, textColor, shape, shapeColor) => {
+  let shapeElement;
+
+  //don't know if i am going to use switch or if statements
+}
+
+Module.exports = generateLogoSvg;
